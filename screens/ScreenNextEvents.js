8742e915ca99c1ce8a1396e83c36050c432c3c8f
Marina Short <mshortg@gmail.com>
2020-04-24 20:04:20 +0200

    Create ScreenNextEvents.js
    Screen Next Events Marina

---
+++ screens/ScreenNextEvents.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ScreenNextEvents = () => {
+  return (
+    <View style={styles.screen}>
+      <Header />
+      <EventsBlock>
+        <Title>Next Events</Title>
+        <View style={styles.listItems}>
+          <TextBody style={styles.item}>14:00   Pick Carol from School</TextBody>
+          <TextBody style={styles.item}>17:00   Spinning Class</TextBody>
+          <TextBody style={styles.itemLast}>20:00   Business Meeting</TextBody>
+          <View style={styles.arrow}><Image source={require('./assets/img/arrow.png')}/></View>
+        </View>
+      </EventsBlock>
+
+      <EventsBlock>
+        <Title>+ Add Event</Title>
+      </EventsBlock>
+    </View>
+  );
+};
+
+export default ScreenOne;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#f33',
+  },
+  text: {
+    fontSize: 150,
+  }
+});
